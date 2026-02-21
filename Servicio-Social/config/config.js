a185734661f50e5ccb59549817d2f10e784e0c9c
webappcap <taquirariandino@gmail.com>
2026-02-20 22:43:56 -0500

    Update config.js

--- Servicio-Social/config/config.js
+++ Servicio-Social/config/config.js
@@ -1,13 +1,19 @@
 // Configuración del sistema
 const CONFIG = {
     // URL de tu Web App de Google Apps Script (backend)
-     BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzdelO9COp-KOIBB8a5tcTJ-YP8OwkmVfRQXXSNMg8HCS6XkCF9eTQRdLGja_GXofr6tw/exec', // Reemplaza con tu URL
+    BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzdelO9COp-KOIBB8a5tcTJ-YP8OwkmVfRQXXSNMg8HCS6XkCF9eTQRdLGja_GXofr6tw/exec',
+    
+    // URL de tu hoja de cálculo de Google Sheets
+    SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1Pc54gL_6UTwDFNSdiDi97p5KhJWLvzPxvEkOE0cevIM/edit?usp=sharing',
     
     // Tiempo de bloqueo después de cada escaneo (ms)
     SCAN_COOLDOWN: 5000,
     
     // Título de la aplicación
     APP_TITLE: 'Labor Social - Liceo Alejandro de Humboldt',
+    
+    // ID de la hoja de cálculo (extraído de la URL)
+    SPREADSHEET_ID: '1Pc54gL_6UTwDFNSdiDi97p5KhJWLvzPxvEkOE0cevIM',
     
     // Colores de la aplicación
     COLORS: {
@@ -16,5 +22,4 @@ const CONFIG = {
         danger: '#c62828',
         success: '#2e7d32'
     }
-
 };
